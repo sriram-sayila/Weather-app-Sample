068dc6a1d7489bdbe5cc8a5b73581946a3ef4c29
sriram-sayila <sriramsayila@gmail.com>
2023-02-28 18:07:20 +0530

    Updates

---
+++ precache-manifest.b408d769f9e009f079f79d28280f4bc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341ba5931129f38352a981d7b7ef6507",
+    "url": "/Weather-app-Sample/index.html"
+  },
+  {
+    "revision": "e6f4c91983c13829df02",
+    "url": "/Weather-app-Sample/static/css/main.aef4b579.chunk.css"
+  },
+  {
+    "revision": "fb579912c1abeb1a5293",
+    "url": "/Weather-app-Sample/static/js/2.20af2123.chunk.js"
+  },
+  {
+    "revision": "e6f4c91983c13829df02",
+    "url": "/Weather-app-Sample/static/js/main.6d904084.chunk.js"
+  },
+  {
+    "revision": "6ab49c40afbebd41907c",
+    "url": "/Weather-app-Sample/static/js/runtime~main.f8813178.js"
+  },
+  {
+    "revision": "4ce11052698e78e13d2e5d60f225bd42",
+    "url": "/Weather-app-Sample/static/media/earth.4ce11052.svg"
+  },
+  {
+    "revision": "543ac1f61ca88692377451ba1046fb2a",
+    "url": "/Weather-app-Sample/static/media/gps-icon.543ac1f6.svg"
+  },
+  {
+    "revision": "cb7ba5e68a82affb9cb250f1ef991b9b",
+    "url": "/Weather-app-Sample/static/media/world.cb7ba5e6.png"
+  }
+]);
